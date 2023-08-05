a3eac47583792233e4a9f9a545dd85d910b01ef4
andres escobar <andres.escobar.godella@gmail.com>
2023-08-05 19:51:13 +0200

    version 0.1

---
+++ src/main/java/cat/itacademy/barcelonactiva/escobarjulia/andres/s04/t01/n03/S04T2N03EscobarJuliaAndresApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.escobarjulia.andres.s04.t01.n03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T2N03EscobarJuliaAndresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04T2N03EscobarJuliaAndresApplication.class, args);
+	}
+
+}
